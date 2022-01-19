3d687d77e7c6bb7303cb3842bac13c4925e4e072
Taichi Uragami <tu.0514.0315@icloud.com>
2022-01-19 19:09:22 +0900

    Initial Commit

---
+++ HandleLocalNotification/HandleLocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandleLocalNotificationApp.swift
+//  HandleLocalNotification
+//
+//  Created by Taichi Uragami on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct HandleLocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
